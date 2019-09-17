a2b61f3fdb1c9ae6573c4ed50980de979e258b52
nvtuanbn <55432217+nvtuanbn@users.noreply.github.com>
2019-09-17 15:05:13 +0700

    Add files via upload

---
+++ react-game/precache-manifest.87a6c249d8e89990a9c6ac41ce25cca7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d896bdf34842a7211b1cdb6f981e1bf",
+    "url": "/nvtuanbn/nvtuanbn.github.io/react-game/index.html"
+  },
+  {
+    "revision": "20d1f2100adc7835b092",
+    "url": "/nvtuanbn/nvtuanbn.github.io/react-game/static/css/main.e16169ae.chunk.css"
+  },
+  {
+    "revision": "ecb7bf58945a0cdb2a12",
+    "url": "/nvtuanbn/nvtuanbn.github.io/react-game/static/js/2.cc424720.chunk.js"
+  },
+  {
+    "revision": "20d1f2100adc7835b092",
+    "url": "/nvtuanbn/nvtuanbn.github.io/react-game/static/js/main.a34daf3d.chunk.js"
+  },
+  {
+    "revision": "e4da1f0f52cf553595c6",
+    "url": "/nvtuanbn/nvtuanbn.github.io/react-game/static/js/runtime~main.b9b358f0.js"
+  }
+]);
---
+++ react-game/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/nvtuanbn/nvtuanbn.github.io/react-game/precache-manifest.87a6c249d8e89990a9c6ac41ce25cca7.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nvtuanbn/nvtuanbn.github.io/react-game/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
